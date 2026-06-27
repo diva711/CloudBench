3c6c343b8aaff74c4652ea39de443fd8ad0e908d
Diva Chandra <divachandra583@gmail.com>
2026-06-27 16:29:18 +0530

    Remove debug file

--- check_constraints.py
+++
@@ -1,6 +0,0 @@
-import psycopg2
-conn = psycopg2.connect(host='localhost', port=5432, database='benchmarkdb', user='postgres', password='Nuclear$583')
-cur = conn.cursor()
-cur.execute("SELECT id, provider, benchmark_type FROM benchmark_results ORDER BY provider, benchmark_type")
-print(cur.fetchall())
-conn.close()
